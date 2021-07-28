0e6b767c9092b0e626998906b6d2b4b9d6051aa9
Haoliang Chang <hlchang4-c@my.cityu.edu.hk>
2021-07-28 13:08:02 +0800

    update codes for Weibo text preprocessing

--- process_text/text_preprocessing.py
+++ process_text/text_preprocessing.py
@@ -2,16 +2,49 @@ from nltk.stem.lancaster import LancasterStemmer
 import re
 import os
 import string
-import spacy
 import zh_core_web_sm
+import emoji
+import pandas as pd
 from spacy.lang.en.stop_words import STOP_WORDS
+from collections import Counter
 
 import data_paths
 
+# Get all the punctuations
 punctuations_all = string.punctuation + '。，﹑？！：；“”（）《》•……【】'
 
+# Load the Chinese language model for preprocessing
 nlp = zh_core_web_sm.load()
-traffic_word_set = {'堵', '拥堵', '车祸', '剐蹭', '事故', '绕行', '追尾', '相撞', '塞车', '路况'}
+
+
+def remove_emojis(text) -> str:
+    """
+    Remove emojis from text
+    :param text: a text list or text string
+    :return: cleaned text list or cleaned text string based on the type of text
+    """
+    assert type(text) == list or type(text) == str, 'The input should be either list or string'
+    if type(text) == list:
+        return [c for c in text if c not in emoji.UNICODE_EMOJI]
+    else:
+        return ''.join(c for c in text if c not in emoji.UNICODE_EMOJI)
+
+
+def create_augment_text_file(dataframe, text_colname, label_colname, save_path, save_filename):
+    """
+    Create the txt file for weibo data augmentation
+    :param dataframe: a pandas dataframe saving the Weibo text
+    :param text_colname: the colname of the column saving the Weibo text
+    :param label_colname: the colname of the column saving the Weibo annotated label
+    :param save_path: the saving path
+    :param save_filename: the saved filename
+    :return: None. The txt file waiting to be augmented is saved to local directory
+    """
+    text_list = list(dataframe[text_colname])
+    label_list = list(dataframe[label_colname])
+    with open(os.path.join(save_path, save_filename), 'w', encoding='utf-8') as file:
+        for text, label in zip(text_list, label_list):
+            file.write(str(label) + '\t' + str(text) + '\n')
 
 
 def create_stopwords_list(stopword_path: str) -> list:
@@ -65,7 +98,7 @@ def preprocessing(raw_tweet, stemming=False, remove_stopwords=False):
     return " ".join(result)
 
 
-def preprocessing_weibo(raw_tweet, tokenization=True, return_word_list=False):
+def preprocessing_weibo(raw_tweet, tokenization=True, return_word_list=True):
     """
     Preprocess the Chinese weibo
     :param raw_tweet: raw Chinese weibo string
@@ -86,7 +119,7 @@ def preprocessing_weibo(raw_tweet, tokenization=True, return_word_list=False):
     stopwords_list = create_stopwords_list(stopword_path=data_paths.chinese_stopword_path)
     words_only = re.sub("[0-9]", "", text_without_username2)
     words_without_puc = re.sub("[{}]".format(punctuations_all), "", words_only)
-    #
+
     # 3. Tokenize the weibos using spacy
     if tokenization:
         doc = nlp(words_without_puc)
@@ -95,11 +128,11 @@ def preprocessing_weibo(raw_tweet, tokenization=True, return_word_list=False):
         words_final = [word for word in words_list if word not in stopwords_list]
 
         if return_word_list:
-            return words_final
+            return [re.sub(r'[^\u4e00-\u9fff]+', '', chinese_words) for chinese_words in words_final]
         else:
-            return ' '.join(words_final)
+            return re.sub(r'[^\u4e00-\u9fff ]+', '', ' '.join(words_final))
     else:
-        return words_without_puc
+        return words_only
 
 
 def preprocessing_traffic_accounts(traffic_weibo):
